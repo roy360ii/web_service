1ba703695b1dc11b6b03d270c5f3128bb5ccf2f9
weslleymart16 <weslleymartins15@gmail.com>
2024-01-25 14:59:44 -0300

    cria banco de dados

---
+++ database/migrations/2024_01_25_014006_create_address_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('address', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            
+            $table->integer('user_id');
+            $table->string('address');
+            $table->string('number');
+            $table->string('complement');
+            $table->string('neighborhood');
+            $table->string('zipcode');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address');
+    }
+}
